99b29477b59733b350a862307887ee8c3bdd032e
anthonybartczak <to0oni456@gmail.com>
2022-07-21 20:52:11 +0200

    Tailwind readjustments

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,111 +1,152 @@
-import Head from 'next/head';
-import { useState } from 'react'
-import Footer from '../../components/Footer';
-import Navbar from '../../components/Navbar';
+import Head from "next/head";
+import { useState } from "react";
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
 
 function Contact() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [messageContent, setMessageContent] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [emailAddress, setEmailAddress] = useState('')
-    const [messageContent, setMessageContent] = useState('')
-    const [submitted, setSubmitted] = useState(false)
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log("Sending");
+    let data = {
+      firstName,
+      lastName,
+      emailAddress,
+      messageContent,
+    };
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault()
-        console.log('Sending')
-        let data = {
-            firstName,
-            lastName,
-            emailAddress,
-            messageContent}
+    alert("Your message has been sent! Thank you :)");
 
-    alert('Your message has been sent! Thank you :)')
+    fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log("Response received");
+      if (res.status === 200) {
+        console.log("Response succeeded!");
+        setSubmitted(true);
+        setFirstName("");
+        setLastName("");
+        setEmailAddress("");
+        setMessageContent("");
+      }
+    });
+  };
 
-    fetch('/api/contact', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      }).then((res) => {
-        console.log('Response received')
-        if (res.status === 200) {
-          console.log('Response succeeded!')
-          setSubmitted(true)
-          setFirstName('')
-          setLastName('')
-          setEmailAddress('')
-          setMessageContent('')
-        }
-    })}
-
-
-    return (
+  return (
     <>
-    <Navbar/>
-        <div className="relative mt-12 mb-8">
-            <div
-                className="mx-4 xl:mx-80 absolute filter -inset-0.5 bg-gradient-to-r from-teal-300 via-blue-400 to-purple-400 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt">
+      <Navbar />
+      <div className="relative mt-12 mb-8">
+        <div className="animate-tilt absolute -inset-0.5 mx-4 rounded-lg bg-gradient-to-r from-teal-300 via-blue-400 to-purple-400 opacity-75 blur filter transition duration-1000 group-hover:opacity-100 group-hover:duration-200 xl:mx-80"></div>
+        <form className="relative mx-4 mt-5 rounded-2xl bg-white px-8 pt-8 pb-1 shadow-2xl dark:bg-gray-800 md:col-span-8 xl:mx-80">
+          <div className="-mx-3 flex flex-wrap">
+            <div className="mb-6 w-full px-3 md:mb-0 md:w-1/2">
+              <label
+                className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
+                htmlFor="grid-first-name"
+              >
+                First Name
+              </label>
+              <input
+                onChange={(e) => {
+                  setFirstName(e.target.value);
+                }}
+                className="mr-3 w-full
+                        appearance-none border-b border-teal-500 bg-transparent p-4 leading-tight text-gray-700 focus:bg-gray-200
+                        focus:outline-none"
+                type="text"
+                placeholder="John"
+                aria-label="Full name"
+              />
+              <p className="mb-5 text-xs italic text-red-500"></p>
             </div>
-            <form
-                className="relative bg-white dark:bg-gray-800 rounded-2xl shadow-2xl md:col-span-8 mt-5 pt-8 pb-1 px-8 mx-4 xl:mx-80">
-                <div className="flex flex-wrap -mx-3">
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                            htmlFor="grid-first-name">
-                            First Name
-                        </label>
-                        <input onChange={(e)=>{setFirstName(e.target.value)}} className="appearance-none bg-transparent
-                        border-b border-teal-500 w-full text-gray-700 mr-3 p-4 leading-tight focus:outline-none
-                        focus:bg-gray-200" type="text" placeholder="John" aria-label="Full name"/>
-                        <p className="text-red-500 text-xs italic mb-5"></p>
-                    </div>
-                    <div className="w-full md:w-1/2 px-3">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                            htmlFor="grid-last-name">
-                            Last Name
-                        </label>
-                        <input onChange={(e)=>{setLastName(e.target.value)}} className="appearance-none bg-transparent
-                        border-b border-teal-500 w-full text-gray-700 mr-3 p-4 leading-tight focus:outline-none
-                        focus:bg-gray-200" type="text" placeholder="Doe" aria-label="Full name"/>
-                    </div>
-                </div>
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full px-3">
-                        <label className="my-10 block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                            htmlFor="grid-email-address">
-                            Email Address
-                        </label>
-                        <input onChange={(e)=>{setEmailAddress(e.target.value)}} className="appearance-none
-                        bg-transparent border-b border-teal-500 w-full text-gray-700 mr-3 p-4 leading-tight
-                        focus:outline-none focus:bg-gray-200" type="email" placeholder="example@exp.com"
-                        aria-label="Full name"/>
-                    </div>
-                </div>
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full px-3">
-                        <label className="my-10 block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                            htmlFor="grid-message-content">
-                            Your Message
-                        </label>
-                        <textarea onChange={(e)=>{setMessageContent(e.target.value)}} rows={10} className="appearance-none bg-transparent border-b border-teal-500 w-full text-gray-700 mr-3 p-4 focus:bg-gray-200 leading-tight focus:outline-none" placeholder="Your message goes here." aria-label="Full name">
-                </textarea>
-                    </div>
-                    <div className="flex justify-between w-full px-3">
-                        <div className="md:flex md:items-center"></div>
-                        <button onClick={(e)=>{handleSubmit(e)}} className="my-2 shadow bg-gray-700 hover:bg-gray-600
-                            focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
-                            type="submit">
-                            Send Message
-                        </button>
-                    </div>
-                </div>
-            </form>
-        </div>
-        <Footer />
+            <div className="w-full px-3 md:w-1/2">
+              <label
+                className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
+                htmlFor="grid-last-name"
+              >
+                Last Name
+              </label>
+              <input
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                }}
+                className="mr-3 w-full
+                        appearance-none border-b border-teal-500 bg-transparent p-4 leading-tight text-gray-700 focus:bg-gray-200
+                        focus:outline-none"
+                type="text"
+                placeholder="Doe"
+                aria-label="Full name"
+              />
+            </div>
+          </div>
+          <div className="-mx-3 mb-6 flex flex-wrap">
+            <div className="w-full px-3">
+              <label
+                className="my-10 mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
+                htmlFor="grid-email-address"
+              >
+                Email Address
+              </label>
+              <input
+                onChange={(e) => {
+                  setEmailAddress(e.target.value);
+                }}
+                className="mr-3
+                        w-full appearance-none border-b border-teal-500 bg-transparent p-4 leading-tight text-gray-700
+                        focus:bg-gray-200 focus:outline-none"
+                type="email"
+                placeholder="example@exp.com"
+                aria-label="Full name"
+              />
+            </div>
+          </div>
+          <div className="-mx-3 mb-6 flex flex-wrap">
+            <div className="w-full px-3">
+              <label
+                className="my-10 mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
+                htmlFor="grid-message-content"
+              >
+                Your Message
+              </label>
+              <textarea
+                onChange={(e) => {
+                  setMessageContent(e.target.value);
+                }}
+                rows={10}
+                className="mr-3 w-full appearance-none border-b border-teal-500 bg-transparent p-4 leading-tight text-gray-700 focus:bg-gray-200 focus:outline-none"
+                placeholder="Your message goes here."
+                aria-label="Full name"
+              ></textarea>
+            </div>
+            <div className="flex w-full justify-between px-3">
+              <div className="md:flex md:items-center"></div>
+              <button
+                onClick={(e) => {
+                  handleSubmit(e);
+                }}
+                className="focus:shadow-outline my-2 rounded bg-gray-700
+                            py-2 px-6 font-bold text-white shadow hover:bg-gray-600 focus:outline-none"
+                type="submit"
+              >
+                Send Message
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <Footer />
     </>
-)}
+  );
+}
 
-export default Contact
+export default Contact;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Navbar from '../components/Navbar'
-import LogoMain from '../components/LogoMain';
-import OurServices from '../components/main/OurServices';
-import AboutUs from '../components/main/AboutUs';
-import Map from '../components/main/Map';
-import Footer from '../components/Footer';
-import Link from 'next/link';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import LogoMain from "../components/LogoMain";
+import OurServices from "../components/main/OurServices";
+import AboutUs from "../components/main/AboutUs";
+import Map from "../components/main/Map";
+import Footer from "../components/Footer";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -18,39 +18,38 @@ const Home: NextPage = () => {
       </Head>
       <Navbar />
       <main>
-        <div
-          className="flex flex-wrap mt-12 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="text-center md:text-left mx-auto xl:mt-16">
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block xl:inline">Centrum Usług</span>{' '}
+        <div className="mx-auto mt-12 flex max-w-7xl flex-wrap px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto text-center md:text-left xl:mt-16">
+            <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+              <span className="block xl:inline">Centrum Usług</span>{" "}
               <span className="block text-indigo-600">Motoryzacyjnych</span>
             </h1>
-            <p
-              className="text-center xl:text-left mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+            <p className="mt-3 text-center text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0 xl:text-left">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat aliqua.
             </p>
-            <div className="mt-5 sm:mt-8 sm:flex justify-center xl:justify-start">
+            <div className="mt-5 justify-center sm:mt-8 sm:flex xl:justify-start">
               <Link href="/contact">
-                <a className="inline-block px-8 py-3 text-xl font-medium text-white transition bg-indigo-600 rounded hover:scale-110 hover:shadow-xl active:bg-indigo-500 focus:outline-none focus:ring">
+                <a className="inline-block rounded bg-indigo-600 px-8 py-3 text-xl font-medium text-white transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:bg-indigo-500">
                   Skontaktuj się z nami!
                 </a>
               </Link>
             </div>
           </div>
-          <div className="flex-initial grow mx-4 pt-6 xl:pt-2 max-w-md justify-center">
+          <div className="mx-4 max-w-md flex-initial grow justify-center pt-6 xl:pt-2">
             <LogoMain />
           </div>
         </div>
-        <AboutUs/>
-        <OurServices/>
-        <Map/>
+        <AboutUs />
+        <OurServices />
+        <Map />
         {/* <MainClients /> */}
       </main>
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
